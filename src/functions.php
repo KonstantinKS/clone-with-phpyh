7399a200f8641cc9445ebac460d5acba8a8f92e7
Konstantin Karpov <k-karpov@demliz.com>
2026-03-26 13:08:04 +0300

    Revome redundant phpdoc

--- src/functions.php
+++ src/functions.php
@@ -7,11 +7,6 @@ namespace Kenny1911\CloneWith;
 use Kenny1911\CloneWith\Exception\CloneException;
 
 /**
- * @param object $object
- * @param array<string, mixed> $properties
- *
- * @return object
- *
  * @psalm-template T
  * @psalm-param T $object
  *
